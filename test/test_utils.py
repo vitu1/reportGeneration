5a7ac2b875c2e38a40756bf21b062e2a95a34d4f
ctanes <ctanes@gmail.com>
2015-12-02 12:39:03 -0500

    Added tests for fastqc reports

--- test/test_utils.py
+++ test/test_utils.py
@@ -16,9 +16,10 @@ class TestJson(unittest.TestCase):
 
     def check_result(self, observed_fp, expected_fp):
         observed = observed_fp.read()
+        print(observed)
         with open(expected_fp) as f_in:
             expected = f_in.read()
-        self.assertTrue(observed, expected)
+        self.assertEqual(observed, expected)
 
     def test_merge_json(self):
         inputs = [
@@ -37,11 +38,23 @@ class TestJson(unittest.TestCase):
         Report.run()
         self.check_result(observed_fp, os.path.join(self.data_dir, 'mergeTsv_results.tsv'))
 
-    def test_fastqc_quality(self):
-        pass
+    def test_fastqc_report(self):
+        Report = MergeFastqc(self.data_dir, "", self.output_dir, "fastqc")
+        Report.run()
+        
+        summary_observed_fp = Report.build_output_path('summary')
+        with open(summary_observed_fp) as f_in:
+            self.check_result(f_in, os.path.join(self.data_dir, 'fastqc_summary_results.tsv'))
+        
+        quality_observed_fp = Report.build_output_path('quality')
+        with open(quality_observed_fp) as f_in:
+            self.check_result(f_in, os.path.join(self.data_dir, 'fastqc_quality_results.tsv'))
 
-    def test_fastqc_summary(self):
-        pass
+    def test_build_path_fastqc(self):
+        Report = MergeFastqc("home/someuser", "before_trim", "home/output_dir", "fastqc")
+        observed = Report.build_output_path('summary')
+        self.assertEqual(observed, "home/output_dir/fastqc_before_trim_summary.tsv")
+
 
 #if __name__ == "__main__":
 #    unittest.main()
